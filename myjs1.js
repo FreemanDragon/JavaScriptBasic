c09eaad44ed63ea4361c4f55a5460c5bfbf8becd
Vladimir <Stepanovvb@yandex.ru>
2019-05-15 01:46:27 +0300

    Сделал шахматную доску

--- myjs1.js
+++ myjs1.js
@@ -1,26 +1,30 @@
-function numberToObject(number) {
-
-    while (1) {
-        if (0 <= number && number % 1 == 0) break;
-        else number = +prompt('Вы ввели некорректное число\nВведите целое число больше 0');
-    }
-
-    if (number > 999) {
-        console.log('Число больше 999');
-        return {};
-    }
-
-    var hundreds = number / 100 - ((number / 100) % 1);
-    var tens = (number - hundreds * 100)/ 10 - (((number - hundreds * 100) / 10) % 1);
-    var units = number - hundreds * 100 - tens * 10;
-
-    return {
-        единицы : units,
-        десятки : tens,
-        сотни : hundreds,
-    }
-}
 function solution() {
-    var numberO = numberToObject(+prompt('Введите целое число больше 0'));
-    console.log(numberO);
+    var $solution = document.getElementById('Sol1');
+    $solution.textContent = '';
+    var $chessboard = document.createElement('table');
+    $chessboard.classList.add('chessboard');
+    var colourCell = ['black','white',];
+    var colourCellIndex;
+
+    for (var i = 0; i < 9; i++) {
+        var $row = document.createElement('tr');
+        if ((9 - i) % 2 == 0) colourCellIndex = true;
+        else colourCellIndex = false;
+
+        for (var j = '@'; j < 'I'; j = String.fromCharCode(j.charCodeAt(0) + 1)) {
+            var $cell = document.createElement('td');
+            if (i == 0 && j !== '@')
+                $cell.textContent = j;
+            else if (i !== 0 && j == '@')
+                $cell.textContent = 9 - i;
+            if (i !== 0 && j !== '@') {
+                $cell.classList.add('chessboard__cell');
+                $cell.classList.add(colourCell[+colourCellIndex]);
+                colourCellIndex = !colourCellIndex;
+            }
+            $row.appendChild($cell);
+        }
+        $chessboard.appendChild($row);
+    }
+    $solution.appendChild($chessboard);
 }
