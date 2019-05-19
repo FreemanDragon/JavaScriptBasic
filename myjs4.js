9f0a8b9682ee3c579071039f6b0d3bb71d285d7c
Vladimir <Stepanovvb@yandex.ru>
2019-05-19 19:52:08 +0300

    Сделал чтобы построение product и корзины не просходило при нажатии на других объектах

--- myjs4.js
+++ myjs4.js
@@ -1,9 +1,17 @@
 function handleButtonClick(event) {
-    if (event.target.textContent == 'Купить')
+    if (event.target.textContent == 'Купить') {
         addGoodsToCart(product[event.target.parentNode.dataset.number]);
-    if (event.target.textContent == 'Удалить')
+        init();
+    }
+    if (event.target.textContent == 'Удалить') {
         cart.splice(event.target.parentNode.dataset.number, 1);
-    init();
+        init();
+    }
+}
+
+function handleInputChange(event) {
+//    if (event.target == 'Input')
+        console.log(event.target);
 }
 
 function init() {
@@ -13,6 +21,7 @@ function init() {
     $product.addEventListener('click', handleButtonClick);
     var $cart = document.querySelector('#cart');
     $cart.addEventListener('click', handleButtonClick);
+    $cart.addEventListener('change', handleInputChange);
 }
 
 window.addEventListener('load', init);
