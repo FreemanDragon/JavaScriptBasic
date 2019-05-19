ac9ee26246b589ea07fcfd662ebd677e8cc40edf
Vladimir <Stepanovvb@yandex.ru>
2019-05-19 18:34:30 +0300

    Добавил резделители строк и Список продуктов

--- myjs2.js
+++ myjs2.js
@@ -1,16 +1,21 @@
 var cart = [];
+
 var boot = {
     name: 'Ботинки',
     price: 2000,
     quantity: 0,
     };
+
 var shirt = {
     name: 'Футболка',
     price: 1000,
     quantity: 0,
     };
+
 var trousers = {
     name: 'Штаны',
     price: 1500,
     quantity: 0,
     };
+
+var product = [boot, shirt, trousers,];
